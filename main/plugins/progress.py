6a659e13bcccc35773a7201b10facd56f7f8ec9f
Harshith Kumar <123058145+Spyder67@users.noreply.github.com>
2024-03-27 11:36:25 +0530

    Update progress.py

--- main/plugins/progress.py
+++ main/plugins/progress.py
@@ -3,8 +3,8 @@ import os
 import time
 import json
 
-FINISHED_PROGRESS_STR = "■"
-UN_FINISHED_PROGRESS_STR = "▢"
+FINISHED_PROGRESS_STR = "🟥"
+UN_FINISHED_PROGRESS_STR = "⬜"
 DOWNLOAD_LOCATION = "/app"
 
 
